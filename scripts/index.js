4c66b87441eca2454abfe3989ce8ccec8c43393b
Abdul-Alim <abdul_halim_95@mail.ru>
2024-04-18 13:37:37 +0300

    test

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,51 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+function addCard(card, cardDelete) {
+  const cloneTemplate = cardTemplate.querySelector('.card').cloneNode(true);
+  const placeList = document.querySelector('.places__list');
+  const cardButton = cloneTemplate.querySelector('.card__delete-button');
+  cardButton.addEventListener('click', cardDelete)
 
-// @todo: Функция создания карточки
+  cloneTemplate.querySelector('.card__image').src = card.link
+  cloneTemplate.querySelector('.card__title').textContent = card.name
 
-// @todo: Функция удаления карточки
+  placeList.append(cloneTemplate)
+}
 
-// @todo: Вывести карточки на страницу
+function cardDelete() {
+  document.querySelector('.card').remove()
+}
+
+for(let i = 0; i < initialCards.length; i++) {
+  addCard(initialCards[i], cardDelete)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
